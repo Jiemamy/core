af5b46145126a65fde7e01d3e4100c15d1e14aa2
MIYAMOTO Daisuke <dai.0304@gmail.com>
2011-01-05 04:16:55 +0000

    [CORE-0]NamingUtil追加

---
+++ jiemamy-core/src/main/java/org/jiemamy/utils/NamingUtil.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2007-2011 Jiemamy Project and the Others.
+ * Created on 2011/01/04
+ *
+ * This file is part of Jiemamy.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.jiemamy.utils;
+
+import org.apache.commons.lang.Validate;
+
+import org.jiemamy.JiemamyContext;
+import org.jiemamy.model.dbo.AbstractDatabaseObjectModel;
+import org.jiemamy.model.dbo.DatabaseObjectModel;
+import org.jiemamy.model.dbo.DomainModel;
+import org.jiemamy.model.dbo.TableModel;
+import org.jiemamy.model.dbo.ViewModel;
+import org.jiemamy.model.dbo.index.IndexModel;
+
+/**
+ * TODO for daisuke
+ * 
+ * @version $Id$
+ * @author daisuke
+ */
+public class NamingUtil {
+	
+	/**
+	 * 自動的にエンティティ名を生成し、設定する。
+	 * 
+	 * @param target 設定対象のエンティティ
+	 * @param context ルートモデル
+	 * @throws IllegalArgumentException 引数に{@code null}を与えた場合
+	 */
+	public static void autoName(AbstractDatabaseObjectModel target, JiemamyContext context) {
+		Validate.notNull(target);
+		Validate.notNull(context);
+		
+		StringBuilder sb = new StringBuilder();
+		int counter = 0;
+		boolean duplicated = true;
+		while (duplicated) {
+			if (target instanceof TableModel) {
+				sb.append("TABLE_");
+			} else if (target instanceof ViewModel) {
+				sb.append("VIEW_");
+			} else if (target instanceof DomainModel) {
+				sb.append("DOMAIN_");
+			} else if (target instanceof IndexModel) {
+				sb.append("INDEX_");
+			} else {
+				throw new IllegalStateException("Unknown DatabaseObjectModel: " + target.getClass().toString());
+			}
+			sb.append(++counter);
+			
+			duplicated = false;
+			for (DatabaseObjectModel dom : context.getDatabaseObjects()) {
+				if (sb.toString().equals(dom.getName())) {
+					duplicated = true;
+					sb.setLength(0);
+					break;
+				}
+			}
+		}
+		
+		target.setName(sb.toString());
+	}
+	
+	private NamingUtil() {
+	}
+	
+}
---
+++ jiemamy-core/src/test/java/org/jiemamy/utils/NamingUtilTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2007-2011 Jiemamy Project and the Others.
+ * Created on 2011/01/04
+ *
+ * This file is part of Jiemamy.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.jiemamy.utils;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.jiemamy.JiemamyContext;
+import org.jiemamy.model.dbo.DefaultTableModel;
+
+/**
+ * TODO for daisuke
+ * 
+ * @version $Id$
+ * @author daisuke
+ */
+public class NamingUtilTest {
+	
+	private JiemamyContext context;
+	
+	private DefaultTableModel t1;
+	
+
+	/**
+	 * テストを初期化する。
+	 * 
+	 * @throws Exception 例外が発生した場合
+	 */
+	@Before
+	public void setUp() throws Exception {
+		context = new JiemamyContext();
+		t1 = new DefaultTableModel(UUID.randomUUID());
+		t1.setName("TABLE_1");
+		context.store(t1);
+	}
+	
+	/**
+	 * TODO for daisuke
+	 * 
+	 * @throws Exception 例外が発生した場合
+	 */
+	@Test
+	public void testname() throws Exception {
+		DefaultTableModel t2 = new DefaultTableModel(UUID.randomUUID());
+		NamingUtil.autoName(t2, context);
+		
+		assertThat(t2.getName(), is(not("TABLE_1")));
+		context.store(t2);
+		
+		DefaultTableModel t3 = new DefaultTableModel(UUID.randomUUID());
+		NamingUtil.autoName(t3, context);
+		
+		assertThat(t3.getName(), is(not("TABLE_1")));
+		assertThat(t3.getName(), is(not(t2.getName())));
+	}
+	
+}
