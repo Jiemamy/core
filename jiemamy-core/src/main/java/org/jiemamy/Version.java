817a6c72d8e1c8274cf60890ffa981a8d5a0b6b9
MIYAMOTO Daisuke <dai.0304@gmail.com>
2011-10-03 13:31:24 +0000

    [CORE-0] バージョン繰り上げ忘れてたー。

--- jiemamy-core/src/main/java/org/jiemamy/Version.java
+++ jiemamy-core/src/main/java/org/jiemamy/Version.java
@@ -28,7 +28,7 @@ import org.apache.commons.lang.Validate;
  */
 public class Version {
 	
-	static final Version INSTANCE = new Version(0, 3, 0, false);
+	static final Version INSTANCE = new Version(0, 3, 2, false);
 	
 
 	/**
